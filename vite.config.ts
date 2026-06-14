40b8122fec5651ca439107aa12192a6cc3724625
marinhoelekaze-wq <marinhoelekaze@gmail.com>
2026-06-13 23:25:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tailwindcss(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    tanstackStart({
-      server: {
-        entry: "server",
-        preset: "vercel", // <-- linha adicionada
-      },
-    }),
     react(),
   ],
   resolve: {
